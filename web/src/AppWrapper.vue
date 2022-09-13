572184a6a8649c5b9083a597e5ffd3cd7606dd81
wzc <zhichao0314@126.com>
2022-09-13 16:38:42 +0800

    web

---
+++ web/src/AppWrapper.vue
@@ -0,0 +1,20 @@
+<template>
+
+  <el-config-provider :locale="locale">
+    <app/>
+  </el-config-provider>
+
+</template>
+
+<script setup lang="ts">
+import {ElConfigProvider} from 'element-plus'
+
+import zhCn from 'element-plus/lib/locale/lang/zh-cn'
+import App from './App.vue';
+
+const locale =  zhCn;
+</script>
+
+<style scoped>
+
+</style>
